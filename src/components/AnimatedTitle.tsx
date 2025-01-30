947d965abd48f07a2df19bbc7241d8e0234abdcc
Roshan Kumar Sahu <igniter909505@gmail.com>
2025-01-30 20:07:05 +0530

    created animated title

---
+++ src/components/AnimatedTitle.tsx
@@ -0,0 +1,56 @@
+import gsap from "gsap";
+import { useEffect, useRef } from "react"
+
+interface AnimatedTitleProps {
+    title: string;
+    containerClass?: string;
+}
+
+const AnimatedTitle = ({title,containerClass}: AnimatedTitleProps) => {
+
+    const containerRef = useRef(null);
+    useEffect(() => {                   
+        const ctx = gsap.context(() => {
+            const titleAnimation = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: "100 bottom",
+                    end: "center bottom",
+                    toggleActions: 'play none none reverse'
+                }
+            });
+            titleAnimation.to(".animated-word",{
+                opacity:1,
+                transform: 'translate3d(0,0,0) rotateY(0deg) rotateX(0deg)',
+                ease: 'power1.inOut',
+                stagger: 0.02
+            })
+        },containerRef);
+        return () => ctx.revert();
+    },[])
+
+    return (
+
+        <div className={`animated-title ${containerClass}`} ref={containerRef}>
+
+            {title.split('<br/>').map((line: string, index: number) => (
+
+                <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+
+                    {line.split(" ").map((word: string, i: number) => (
+
+                        <span key={i} className="animated-word" dangerouslySetInnerHTML={{ __html: word }} />
+
+                    ))}
+
+                </div>
+
+            ))}
+
+        </div>
+
+    )
+
+}
+
+export default AnimatedTitle
